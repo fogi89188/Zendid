a32353fe644b4a02176f88466d8d8a8280eda679
unknown <viktorpetrov02@abv.bg>
2020-07-30 19:20:05 +0300

    minor bug fixes

--- ZendidClient/Zendid/ViewModels/ChatMessageListItemViewModel.cs
+++ ZendidClient/Zendid/ViewModels/ChatMessageListItemViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Google.Protobuf.WellKnownTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZendidCommons;
+using Zendid.Models;
 
 namespace Zendid.ViewModels
 {
@@ -11,8 +14,11 @@ namespace Zendid.ViewModels
         /// <summary>
         /// a view model for each chat message thread item in a chat thread
         /// </summary>
-        public string SenderName { get; set; }
-        public DateTimeOffset MessageSentTime  { get; set; }
-        public string Message { get; set; }
+
+        public static Message serverMessage = new Message();
+
+        public string SenderName { get { return serverMessage.UserSender; } set {serverMessage.UserSender = value; } }
+        public DateTimeOffset MessageSentTime  { get { return serverMessage.Time; } set { serverMessage.Time = value; } }
+        public string Message { get { return serverMessage.MessageStr; } set { serverMessage.MessageStr = value; } }
     }
 }
