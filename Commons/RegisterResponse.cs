e1dda8a84b10e2ac634eef0f5f16c871680b643a
Nikola Pavlovski <nikola.j.pavlovski@outlook.com>
2020-07-30 20:22:43 +0300

    Added server time

--- Commons/RegisterResponse.cs
+++ Commons/RegisterResponse.cs
@@ -10,5 +10,6 @@ namespace ZendidCommons
         public string Status { get; set; }
         public ZendidErrorCodes ErrorCode { get; set; }
         public string Token { get; set; }
+        public DateTime TimeOfLastUpdate { get; set; }
     }
 }
