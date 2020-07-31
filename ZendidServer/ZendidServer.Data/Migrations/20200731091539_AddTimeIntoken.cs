9ed8632ad19378326d5813d9a20aebb552518303
Nikola Pavlovski <nikola.j..pavlovski@gmail.com>
2020-07-31 12:17:38 +0300

    Add migration time in token

---
+++ ZendidServer/ZendidServer.Data/Migrations/20200731091539_AddTimeIntoken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ZendidServer.Data.Migrations
+{
+    public partial class AddTimeIntoken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TimeOfLastUpdate",
+                table: "Tokens",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeOfLastUpdate",
+                table: "Tokens");
+        }
+    }
+}
